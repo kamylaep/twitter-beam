2d09c10c548411b5b57ae5e9d79e5f0343b22dc4
Kamyla Estuqui Parrado <kamylaep@gmail.com>
2020-02-25 18:14:46 -0300

    Refactor json parser to use JsonObject

--- src/main/java/com/kep/beam/pipeline/GetUserDataDoFn.java
+++ src/main/java/com/kep/beam/pipeline/GetUserDataDoFn.java
@@ -6,19 +6,20 @@ import java.util.Map;
 import org.apache.beam.sdk.transforms.DoFn;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 
 public class GetUserDataDoFn extends DoFn<String, Map<String, String>> {
 
   @ProcessElement
   public void processElement(@Element String jsonTweet, OutputReceiver<Map<String, String>> outputReceiver) {
     Gson gson = new Gson();
-    Map<String, Object> tweet = gson.fromJson(jsonTweet, Map.class);
-    Map<String, Object> user = (Map<String, Object>) tweet.get("user");
+    JsonObject tweet = gson.fromJson(jsonTweet, JsonObject.class);
+    JsonObject user = tweet.getAsJsonObject("user");
 
     Map<String, String> out = new HashMap<>();
-    out.put("id", user.get("id").toString());
-    out.put("screen_name", user.get("screen_name").toString());
-    out.put("followers_count", user.get("followers_count").toString());
+    out.put("id", user.get("id").getAsString());
+    out.put("screen_name", user.get("screen_name").getAsString());
+    out.put("followers_count", user.get("followers_count").getAsString());
     outputReceiver.output(out);
   }
 }
